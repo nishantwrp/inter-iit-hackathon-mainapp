5853930f8e4f2c7599e20916727d62410f92e451
YashJipkate <yashjipkate@gmail.com>
2019-12-20 15:43:59 +0530

    Login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginRegisterComponent } from './login-register/login-register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginRegisterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
