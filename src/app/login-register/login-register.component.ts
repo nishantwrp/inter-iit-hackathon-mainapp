2a97665ad0a4faedf28923fdec559b0981fb1976
YashJipkate <yashjipkate@gmail.com>
2019-12-20 16:41:27 +0530

    Login register completed

--- src/app/login-register/login-register.component.ts
+++ src/app/login-register/login-register.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login-register',
   templateUrl: './login-register.component.html',
-  styleUrls: ['./login-register.component.scss']
+  styleUrls: ['./login-register.component.css']
 })
 export class LoginRegisterComponent implements OnInit {
 
